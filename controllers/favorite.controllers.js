627f5e99748851ef9cad7ebb9e9a51c4095e974f
mrsemas10 <mrsemas10@gmail.com>
2024-08-29 10:46:43 -0400

    update fav controllers

---
+++ controllers/favorite.controllers.js
@@ -0,0 +1,42 @@
+import Favorite from '../models/favorite.model';
+
+const addFavorite = async (req, res) => {
+  try {
+    const find = await Favorite.findOne({
+      location: { $regex: new RegExp(req.body.location, "i") },
+      user: { $regex: new RegExp(req.body.user, "i") },
+    });
+    if (find) {
+      return res
+        .status(409)
+        .json({ message: "Location already exists in favorite" });
+    }
+    const favorite = await Favorite.create({
+      ...req.body,
+    });
+    res.status(201).json({ favorite, message: "Location added successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const getAllFavorites = async (req, res) => {
+  const userEmail = req.params.userEmail;
+  try {
+    const favorites = await Favorite.find({ user: userEmail });
+    res.status(200).json(favorites);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching data" });
+  }
+};
+
+const deleteFavorite = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Favorite.findByIdAndDelete(id);
+    res.status(200).json({ message: "Location removed successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting favorite" });
+  }
+};
+
+module.exports = {addFavorite, getAllFavorites, deleteFavorite};
