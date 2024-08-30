94f265c63d0f08bbd0d376fb8757a1a57ee88211
mrsemas10 <mrsemas10@gmail.com>
2024-08-30 10:33:58 -0400

    update models

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -14,6 +15,21 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+//HASH PASSWORD
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt); //save encrypted password in database
+  }
+  next();
+});
+
+// COMPARE PASSWORD
+userSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
 
 const User = mongoose.models?.User || mongoose.model("User", userSchema);
 
